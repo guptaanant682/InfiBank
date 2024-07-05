763d003dcc246b6cf343fbb759862cceaa42f688
guptaanant682 <guptaanant682@gmail.com>
2024-07-05 23:20:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import styles from './style'
+import Hero from './Components/Hero'
+import Navbar from './Components/Navbar'
+import Stats from './Components/Stats'
+import Business from './Components/Business'
+import Billing from './Components/Billing'
+import CardDeal from './Components/CardDeal'
+import Testimonials from './Components/Testimonials'
+import CTA from './Components/CTA'
+import Clients from './Components/Clients'
+import Footer from './Components/Footer'
+
+
+const App = () => {
+  return (
+    <div className='bg-primary w-full overflow-hidden'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar/>
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero/>
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats/> 
+          <Business/>
+          <Billing/> 
+          <CardDeal/> 
+          <Testimonials/>
+          <Clients/>
+          <CTA/>
+          <Footer/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
